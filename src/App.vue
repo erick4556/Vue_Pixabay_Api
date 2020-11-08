4e36f15bd3a6d52dab5030dea97758de508ffc7a
hideveloper20 <electronicmade9064@outlook.com>
2020-11-08 11:49:54 -0500

    Vuetify implementation

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,47 @@
 <template>
-  <div id="app">
-    <!--  <Home v-if="false" />
-    <Pixabay /> -->
-    <router-link to="/">Home</router-link> |
-    <router-link to="/pixa">Imágenes</router-link>
+  <v-app>
+    <v-system-bar>
+      <v-app-bar-nav-icon @click.stop="showMenu = !showMenu" />
+    </v-system-bar>
+    <v-navigation-drawer
+      v-model="showMenu"
+      expand-on-hover
+      temporary
+      absolute
+      mini-variant="miniVariant"
+    >
+      <v-list>
+        <v-list-item class="px-2">
+          <v-list-item-avatar>
+            <v-img src="./assets/logo.png"></v-img>
+          </v-list-item-avatar>
+        </v-list-item>
 
-    <!-- Aqui se va a pintar las rutas -->
-    <router-view />
-  </div>
+        <v-list-item link>
+          <v-list-item-content>
+            <v-list-item-title class="title">Uso de Vue </v-list-item-title>
+            <v-list-item-subtitle>test@gmail.com</v-list-item-subtitle>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+
+      <v-divider></v-divider>
+
+      <v-list nav dense>
+        <div v-for="m in menu" :key="m.nombre">
+          <router-link :to="m.ruta">
+            <v-list-item link>
+              <v-list-item-icon>
+                <v-icon>{{ m.icono }}</v-icon>
+              </v-list-item-icon>
+              <v-list-item-title>{{ m.nombre }}</v-list-item-title>
+            </v-list-item>
+          </router-link>
+        </div>
+      </v-list>
+    </v-navigation-drawer>
+    <router-view></router-view>
+  </v-app>
 </template>
 
 <script>
@@ -19,8 +53,29 @@ export default {
     /* Home,
     Pixabay, */
   },
+  data() {
+    return {
+      menu: [
+        {
+          ruta: "/",
+          nombre: "Inicio",
+          icono: "mdi-folder",
+        },
+        {
+          ruta: "/pixa",
+          nombre: "Imágenes",
+          icono: "mdi-folder",
+        },
+      ],
+      showMenu: false,
+      miniVariant: true,
+    };
+  },
 };
 </script>
 
-<style >
+<style>
+a:link {
+  text-decoration: none;
+}
 </style>
